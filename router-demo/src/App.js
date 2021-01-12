676a689d3904dce8d4cd746b8f7d4aa9856c8dac
yukiice <58852690+yukiice@users.noreply.github.com>
2021-01-12 23:20:15 +0800

    ....

--- router-demo/src/App.js
+++ router-demo/src/App.js
@@ -1,24 +1,43 @@
+import React, { PureComponent } from 'react'
 import "./App.css";
-import { BrowserRouter, Link, Route } from "react-router-dom";
-import About from './components/About.jsx'
-import Home from './components/Home'
-import Profile from './components/Profile'
-function App() {
-  return (
-    <div>
+// Link,
+
+
+import { BrowserRouter,  Route, NavLink,Switch,withRouter} from "react-router-dom";
+import About from "./components/About.jsx";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import User from './components/User'
+import NoMatch from './components/NoMatch'
+class App extends PureComponent {
+  btnClick(){
+    this.props.history.push('/nomatch')
+  }
+  render() {
+    return (
+      <div>
+      <button onClick={()=>this.btnClick()}>点击进行跳转</button>
       <BrowserRouter>
-        <Link to="/">首页</Link>
-        <br/>
+        <NavLink to="/" activeClassName="hover" exact>首页</NavLink>
+        <NavLink to="/about" activeClassName="hover">关于</NavLink>
+        <NavLink to="/profile" activeClassName="hover">我的</NavLink>
+        {/* <Link to="/"> 首页</Link>
+        <br />
         <Link to="/about">关于</Link>
-        <br/>
+        <br />
         <Link to="profile">我的</Link>
-        <br/>
-        <Route exact path="/" component={Home}></Route>
+        <br /> */} 
+       <Switch>
+       <Route exact path="/" component={Home}></Route>
         <Route path="/about" component={About}></Route>
         <Route path="/profile" component={Profile}></Route>
+        <Route></Route>
+       </Switch> 
       </BrowserRouter>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+export default withRouter(App);
