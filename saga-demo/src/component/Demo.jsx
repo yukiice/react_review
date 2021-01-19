441e9e4689a546cfec6606f54715cbf886305d24
yukiice <58852690+yukiice@users.noreply.github.com>
2021-01-19 18:06:03 +0800

    补充文档

--- saga-demo/src/component/Demo.jsx
+++ saga-demo/src/component/Demo.jsx
@@ -3,6 +3,10 @@ import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { Demos } from "./style";
 import { delAction, addAction } from "../redux/Demo/actionCreators";
+import {
+  delFiveAction,
+  addFiveAction,
+} from "../redux/CountFive/actionCreators";
 class Demo extends PureComponent {
   render() {
     return (
@@ -14,17 +18,12 @@ class Demo extends PureComponent {
         <Button onClick={() => this.props.addCountClick()}>增加</Button>
 
         <h2 className="mt1">这里是+5的count展示</h2>
-        
-        <div className="cursor-wait ...">
-            {
-                this.props.countFive
-            }
-        </div>
-        <br/>
-        <Button onClick={() => this.props.delCountFiveClick()}>减少</Button>
+
+        <div className="cursor-wait ...">{this.props.countFive}</div>
         <br />
-        <Button onClick={() => this.props.addCountFiveClick()}>增加</Button>
-        
+        <Button onClick={() => this.props.delCountFiveClick(5)}>减少</Button>
+        <br />
+        <Button onClick={() => this.props.addCountFiveClick(5)}>增加</Button>
       </Demos>
     );
   }
@@ -33,7 +32,7 @@ class Demo extends PureComponent {
 const mapStateToProps = (state) => {
   return {
     count: state.countInfo.count,
-    countFive:state.countFiveInfo.count
+    countFive: state.countFiveInfo.count,
   };
 };
 
@@ -44,7 +43,14 @@ const mapDispatchToProps = (dispatch) => ({
   delCountClick() {
     dispatch(delAction);
   },
-  
+  delCountFiveClick(num) {
+    dispatch(delFiveAction(num));
+  },
+  addCountFiveClick(num) {
+    dispatch(addFiveAction(num));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Demo);
+
+
